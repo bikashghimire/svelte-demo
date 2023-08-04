62f770d67cb5657db7af8a60e5c6c3d3c5b775fe
bikash ghimire <bikashghimire@mogroup.com>
2023-08-04 11:54:27 +0300

    this commits implements search input svelte images items

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	import { onMount } from "svelte";
+	import CastList from "./CastList.svelte";
+	import CastSearch from "./CastSearch.svelte";
+	let searchTerm = "";
+	let cast = [];
+	let castList = [];
+	function filterList(list, query) {
+	  return list.filter((item) => {
+		return item.name.toLowerCase().match(query.toLowerCase());
+	  });
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  
+	onMount(async () => {
+	  const res = await fetch(`cast.json`);
+	  cast = await res.json();
+	  castList = cast;
+	});
+  </script>
+  
+  <main class="container">
+	<CastSearch
+	  bind:searchTerm
+	  on:updateSearch={() => {
+		castList = filterList(cast, searchTerm);
+	  }}
+	/>
+	<CastList cast={castList} />
+  </main>
